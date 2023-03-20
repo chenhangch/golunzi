0d8cd69708238c69028258a30f360cdd5d04f8b1
chenhang <1340938223@qq.com>
2023-03-20 21:00:07 +0800

    style: 将app.go的NewApp()以及run()函数迁移到main.go

--- cli/bestEx/main.go
+++ cli/bestEx/main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
-	"fmt"
-
+	"github.com/hangcodebug/golunzi/cli"
 	"github.com/hangcodebug/golunzi/cli/bestEx/appoptions"
 )
 
+func NewApp(basename string) *cli.AppCli {
+	opts := appoptions.NewOptions()
+	applilcation := cli.NewAppCli(
+		basename,
+		"APPCLI",
+		cli.WithOptions(opts),
+		cli.WithDescription("desc"),
+		cli.WithRunFunc(run(opts)),
+		cli.WithConfig(false),
+	)
+	return applilcation
+}
+
+func run(opts *appoptions.Options) cli.RunFunc {
+	return func(basename string) error {
+		// log.Init(opts.Log)
+		// defer log.Flush()
+
+		// cfg, err := config.CreateConfigFromOptions(opts)
+		// if err != nil {
+		// 	return err
+		// }
+
+		// return Run(cfg)
+		return nil
+	}
+}
 
 func main() {
 	NewApp("test").Run()
-
-	fmt.Println(appoptions.NewOptions().MySQLOptions)
 }
+
