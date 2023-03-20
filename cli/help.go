4212741b8a62cd4a99b384b140cccf3a94d89f13
chenhang <1340938223@qq.com>
2023-03-20 18:34:24 +0800

    feat: 添加help命令行以及help参数

--- cli/help.go
+++ cli/help.go
@@ -1,6 +1,13 @@
 package cli
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+)
 
 const (
 	flagHelp = "help"
@@ -14,8 +21,25 @@ func helpCommand(name string) *cobra.Command {
 		Long: `Help providesn help for any cmmand in the applicaton.
 		Simply type ` + name + `help [path to command] for full detatils`,
 
-		Run: func(cmd *cobra.Command, args []string) {
-			
+		Run: func(c *cobra.Command, args []string) {
+			cmd, _, e := c.Root().Find(args)
+			if cmd == nil || e != nil {
+				c.Printf("Unknown help topic %#q\n", args)
+				_ = c.Root().Usage()
+			} else {
+				cmd.InitDefaultHelpFlag() // make possible 'help' flag to be shown
+				_ = cmd.Help()
+			}
 		},
 	}
+}
+
+// addHelpCommandFlag 添加帮助命令行参数
+func addHelpCommandFlag(usage string, fs *pflag.FlagSet)  {
+	fs.BoolP(
+		flagHelp,
+		flagHelpShorthand,
+		false,
+		fmt.Sprintf("Help for the %s command.", color.GreenString(strings.Split(usage, " ")[0])),
+)
 }
