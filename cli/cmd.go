b64755200e48fba1c9542480d43fd1b0656e32e1
chenhang <1340938223@qq.com>
2023-03-20 18:37:20 +0800

    feat: 增加添加子命令行函数

--- cli/cmd.go
+++ cli/cmd.go
@@ -18,7 +18,6 @@ type Command struct {
 	command  []*Command
 	
 	args  cobra.PositionalArgs // Args 个数设置
-	cmd   *cobra.Command
 	runFunc   RunCommandFunc
 }
 
@@ -29,6 +28,16 @@ type CommandOption func(*Command)
 // RunCommandFunc 相关的 action 
 type RunCommandFunc func(args []string) error
 
+// AddCommand 添加子命令行
+func (c *Command) AddCommand(cmd *Command)  {
+	c.command = append(c.command, cmd)
+}
+
+func (c *Command) AddCommands(cmds ...*Command) {
+	c.command = append(c.command, cmds...)
+}
+
+// BuildCobraCommand 构建命令行及其子命令行
 func (c *Command) BuildCobraCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use: c.usage,
@@ -38,6 +47,7 @@ func (c *Command) BuildCobraCommand() *cobra.Command {
 		// Run: c.runFunc,
 	}
 
+	cmd.SetOut(os.Stdout)
 	// 判断该命令行是否存在子命令行
 	if len(c.command) > 0 {
 		for _, command := range c.command {
@@ -56,7 +66,7 @@ func (c *Command) BuildCobraCommand() *cobra.Command {
 		}
 	}
 
-	//TODO: 添加help命令行
+	addHelpCommandFlag(c.usage, cmd.Flags())
 	return cmd
 }
 
